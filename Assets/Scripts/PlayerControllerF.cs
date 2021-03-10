65a3e106d58f0bb9c564184b7f2a5dc4ecc58a9f
behlül uysal <mockingjaytes@gmail.com>
2021-03-11 00:58:52 +0300

    Added walk animation

---
+++ Assets/Scripts/PlayerControllerF.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerF : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject _player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            _player.transform.position += new Vector3(2, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _player.transform.position += new Vector3(-2, 0, 0);
+        }
+    }
+}
